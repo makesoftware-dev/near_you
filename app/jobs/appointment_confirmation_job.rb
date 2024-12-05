e58b95762602cc577188f19e187dd84552ddcda8
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-12-05 18:57:34 +0200

    Add background jobs to call notifications system

--- app/jobs/appointment_confirmation_job.rb
+++ app/jobs/appointment_confirmation_job.rb
@@ -4,7 +4,12 @@ class AppointmentConfirmationJob < ApplicationJob
   def perform(appointment_id)
     appointment = Appointment.find(appointment_id)
 
+    # Send emails
     AppointmentMailer.confirmation_email_user(appointment).deliver_now
     AppointmentMailer.confirmation_email_provider(appointment).deliver_now
+
+    # Send notifications
+    AppointmentConfirmationNotifier.with(appointment: appointment).deliver(appointment.user)
+    AppointmentConfirmationNotifier.with(appointment: appointment).deliver(appointment.provider.user)
   end
 end
