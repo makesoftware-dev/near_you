cd858a9b5326619e1656b6bca22e530527365cc8
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-12-20 18:45:21 +0200

    Add profile picture to user and provider and carousel images to profider profile

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,9 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_picture])
+  end
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :notifications, as: :recipient, dependent: :destroy, class_name: "Noticed::Notification"
   has_many :reviews, dependent: :destroy
+  has_one_attached :profile_picture
+  # has_many_attached :images
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
