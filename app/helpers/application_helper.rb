cb90da46c4d4ed7026bfc099a920c25bf2af233b
gabrielgiroe1 <gabriel.giroe@gmail.com>
2024-12-24 11:55:31 +0200

    Add link to provider location using google maps

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   #   gravatar_id = Digest::MD5.hexdigest(email.downcase)
   #   "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon"
   # end
+
+  def google_maps_directions_url(location)
+    "https://www.google.com/maps/dir/?api=1&destination=#{CGI.escape(location)}"
+  end
 end
