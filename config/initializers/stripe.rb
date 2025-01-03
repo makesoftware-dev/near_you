919249cd5b6751052d83fb35673e954d60c50659
gabrielgiroe1 <gabriel.giroe@gmail.com>
2025-01-03 17:22:53 +0200

    wdk

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,5 @@
 Rails.configuration.stripe = {
-  publishable_key: Rails.application.credentials.stripe[:STRIPE_PUBLIC_KEY],
-  secret_key: Rails.application.credentials.stripe[:STRIPE_SECRET_KEY]
+  publishable_key: Rails.application.credentials.dig(:stripe, :STRIPE_PUBLIC_KEY) || ENV["STRIPE_PUBLIC_KEY"],
+  secret_key: Rails.application.credentials.dig(:stripe, :STRIPE_SECRET_KEY) || ENV["STRIPE_SECRET_KEY"]
 }
-
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
